31871a91f029a2eeb77c6503fb971f73808997cc
roger <rogervtsc@hotmail.com>
2023-10-24 19:44:23 -0300

    feat: add planet entity

---
+++ src/entity/Planet.ts
@@ -0,0 +1,18 @@
+class Planet {
+  id: string | undefined;
+
+  name: string | undefined;
+
+  weather: string| undefined;
+
+  terrain: string| undefined;
+
+  apparitions?: number;
+
+  created_at: Date| undefined;
+
+  updated_at: Date| undefined;
+  
+}
+
+export { Planet };

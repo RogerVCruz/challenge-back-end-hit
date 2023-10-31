26f983b90a590c300a9ae71dc00c44acb7c4eb9e
roger <rogervtsc@hotmail.com>
2023-10-30 22:04:58 -0300

    feat: add User entity

---
+++ src/modules/accounts/infra/prisma/entities/User.ts
@@ -0,0 +1,17 @@
+
+
+class User {
+  id: string | undefined;
+
+  name: string | undefined;
+
+  password: string | undefined;
+
+  email: string | undefined;
+
+  created_at: Date | undefined;
+
+  updated_at: Date | undefined;
+}
+
+export { User };
